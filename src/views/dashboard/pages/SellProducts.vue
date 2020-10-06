c5e59f4e7f987812cba3e52875709762452ebca5
intelligentfarmingplatform <71752993+intelligentfarmingplatform@users.noreply.github.com>
2020-10-06 21:25:10 +0700

    next

--- src/views/dashboard/pages/SellProducts.vue
+++ src/views/dashboard/pages/SellProducts.vue
@@ -11,6 +11,8 @@
         >
           <v-card-title>
             <v-card
+                v-for="items in showproducts "
+                :key="items"
                 :loading="loading"
                 class="mx-5 cardproducts"
                 max-width="250"
@@ -18,21 +20,21 @@
             >
                 <v-img
                 height="150"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                :src="items.productimg"
                 ></v-img>
 
-                <v-card-title class="mt-5">ชื่อสินค้า</v-card-title>
+                <v-card-title class="mt-5 mx-auto">{{ items.productname }}</v-card-title>
                     <v-list-item>
-                        <v-list-item-title>ราคา บาท</v-list-item-title>
-                        <v-list-item-subtitle class="text-right">จำนวน</v-list-item-subtitle>
+                        <v-list-item-title>{{ items.productprice }} บาท</v-list-item-title>
+                        <v-list-item-subtitle class="text-right">{{ items.productnumber }}</v-list-item-subtitle>
                     </v-list-item>
                 <v-card-text class="mx-0 my-0">
-                <div class="detailproducts">Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+                <div class="detailproducts">{{ items.productdetail }}</div>
                 </v-card-text>
                 <v-divider class="mx-1"></v-divider>
                 <v-card-title>Tab</v-card-title>
                 <v-card-text class="mx-0 my-0">
-                <div class="tabproducts">Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+                <div class="tabproducts">{{ items.producttab }}</div>
                 </v-card-text>
                 <v-card-actions>
                 <v-btn
@@ -56,27 +58,133 @@
                 </v-btn>
                 </v-card-actions>
             </v-card>
-            <v-card
-                :loading="loading"
-                class="mx-5 newcardproducts d-flex "
-                width="250"
-                height="450"
-                color="info"
-            >
-                <v-card-text class=" d-flex my-auto justify-center flex-column">
-                    <v-icon
-                        size="55px"
-                        color="#ffffff"
-                    >
-                        mdi-plus
-                    </v-icon>
-                    <v-card-title 
-                        class=" justify-center"
-                    >
-                        New Products
-                    </v-card-title>
-                </v-card-text>
-            </v-card>
+            <!-- ----------------------------------------------------end cardshow----------------------------------- -->
+                <v-dialog
+                  v-model="dialog"
+                  max-width="500px"
+                >
+                    <template v-slot:activator="{ on, attrs }">                
+                        <v-card
+                            class="mx-5 newcardproducts d-flex "
+                            width="250"
+                            height="450"
+                            color="info"
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                            <v-card-text class=" d-flex my-auto justify-center flex-column">
+                                <v-icon
+                                    size="55px"
+                                    color="#ffffff"
+                                >
+                                    mdi-plus
+                                </v-icon>
+                                <v-card-title 
+                                    class=" justify-center"
+                                >
+                                    New Products
+                                </v-card-title>
+                            </v-card-text>
+                        </v-card>
+                    </template>
+                    <v-card>
+                        <v-card-title class="headline">
+                        <span>{{ formTitle }}</span>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-container>
+                            <v-form
+                                ref="form"
+                                v-model="isValid"
+                                @submit.prevent="onsubmit"
+                            >
+                                <v-text-field
+                                v-if="editedIndex != -1"
+                                label="ชื่อผู้ใช้งาน"
+                                prepend-icon="mdi-pencil"
+                                disabled
+                                required
+                                ></v-text-field>
+                            
+                                <v-text-field
+                                label="Username"
+                                :rules="[v => !!v || 'กรุณาใส่ชื่อผู้ใช้']"
+                                required
+                                prepend-icon="mdi-pencil"
+                                ></v-text-field>
+                            <v-row>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="6"
+                                >
+                                    <v-text-field
+                                    label="Password"
+                                    :rules="passwordRules"
+                                    maxlength="10"
+                                    :counter="10"
+                                    required
+                                    prepend-icon="mdi-key"
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="6"
+                                >
+                                    <v-text-field
+                                    type="email"
+                                    label="Email"
+                                    :rules="[ v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'กรุณาใส่อีเมล์']"
+                                    required
+                                    prepend-icon="mdi-email"
+                                    ></v-text-field>
+                                </v-col>
+                            </v-row>
+                            
+                                <v-select
+                                
+                                :items="itemsstatus"
+                                :rules="[v => !!v || 'กรุณาเลือกสถานะ']"
+                                label="สถานะ"
+                                required
+                                prepend-icon="mdi-text-account"
+                                ></v-select>
+
+                                <v-checkbox
+                                :label="[`ยืนยันการ${formTitle}`]"
+                                :rules="[v => !!v || `กรุณากดยืนยันการ${formTitle}`]"
+                                ></v-checkbox>
+                                <v-container>
+                                    <v-col
+                                        cols="12"
+                                        align="center"
+                                    >
+                                    <v-btn
+                                    :disabled="!isValid"
+                                    depressed
+                                    color="success"
+                                    class="rounded-pill"
+                                    type="submit"
+                                    >
+                                    บันทึกข้อมูล
+                                    </v-btn>
+
+                                    <v-btn
+                                    depressed
+                                    color="error"
+                                    class="rounded-pill"
+                                    @click="close"
+                                    >
+                                    ย้อนกลับ
+                                    </v-btn>
+                                    </v-col>
+                                </v-container>
+                            </v-form>
+                            </v-container>
+                        </v-card-text>
+                    </v-card>
+                </v-dialog>
           </v-card-title>
         </base-material-card>
       </v-col>
@@ -84,15 +192,44 @@
 </template>
 
 <script>
+import Axios from 'axios';
 export default {
-    name:"Sellproducts",
-    products:{
-        img:'',
-        name:'',
-        stock:'',
-        pirec:'',
-        detail:'',
-    }    
+    name:"sellproducts",
+    data() {
+        return {
+            loading:'',
+            dialog: false,
+            products:{
+                img:'',
+                name:'',
+                stock:'',
+                pirec:'',
+                detail:'',
+            },
+            showproducts:[],    
+        }
+    },
+     created() {
+       Axios.get("http://localhost:3000/api/sellproducts",{ responseType: 'arrybuffer'})
+        .then(response => {
+            this.showproducts = response.data.data ;
+            this.loading = false;
+            this.showproducts.img = 'data:image/jpeg;base64,' + btoa(response.data.results[0].image.data);
+        }).catch((err) => {
+        console.log(err); 
+        })
+    console.log("SellProducts");
+    },
+    methods:{
+        close () {
+        this.$nextTick(() => {
+        //   this.editedItem = Object.assign({}, this.defaultItem)
+        //   this.editedIndex = -1
+          this.dialog = false
+          this.$refs.form.reset()
+        })
+      },
+    }
 }
 </script>
 
